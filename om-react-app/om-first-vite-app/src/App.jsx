5b0a16c08d0981f07fa787d864f50c268b086890
bhoyarom005-netizen <bhoyarom005@gmail.com>
2026-05-31 10:09:01 +0530

    Using React

---
+++ om-react-app/om-first-vite-app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+function App(){
+
+let count = 7
+function Greet(){
+  <alert>yes</alert>
+}
+  return(
+    <div>
+      <h1>Hello Guys</h1>
+      <h2>Likes❤️ {count}</h2>
+      <button onClick={Greet}> Good Morning</button>
+    </div>
+  )
+}
+export default App
